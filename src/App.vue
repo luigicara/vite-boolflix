cb4aeac5868a9616c50427cb1b14daf4ba7b6ebf
luigicara <luigi.caravelli98@gmail.com>
2022-12-19 15:05:42 +0100

    improved code, added search for cast and genres and added flags as language

--- src/App.vue
+++ src/App.vue
@@ -10,20 +10,75 @@ export default {
   },
   methods: {
     searchContent() {
-      let movie = `${store.apiURL}search/movie/?${store.apiKey}&language=it_IT&query=${store.searchString}`;
+      let movie = this.urlGen('search', 'movie');
 
-      let tv = `${store.apiURL}search/tv/?${store.apiKey}&language=it_IT&query=${store.searchString}`;
+      let tv = this.urlGen('search', 'tv');
 
       const requestMovie = axios.get(movie);
       const requestTv = axios.get(tv);
 
       axios.all([requestMovie, requestTv]).then(axios.spread((...res) => {
-        store.searchResultMovie = res[0].data.results;
-        store.searchResultTV = res[1].data.results;
+        store.searchResult = res[0].data.results.concat(res[1].data.results)
+
       })).catch(errors => {
         console.log(errors);
+      }).then(() => {
+        this.getCastAndGenres();
       })
+    },
+
+    getCastAndGenres() {
+      const media = store.searchResult;
+
+      for (let i = 0; i < media.length; i++) {
+
+        let cast = this.urlGen('', 'movie', '/credits', media[i].id);
+
+        let genres = this.urlGen('', 'movie', '', media[i].id);
+
+        if (!media[i].hasOwnProperty('title')) {
+          cast = this.urlGen('', 'tv', '/credits', media[i].id);
+
+          genres = this.urlGen('', 'tv', '', media[i].id);
+        }
+
+        const requestCast = axios.get(cast);
+        const requestGenres = axios.get(genres);
+
+        axios.all([requestCast, requestGenres]).then(axios.spread((...res) => {
+
+          media[i].cast = res[0].data.cast.splice(0, 5);
+          media[i].genres = res[1].data.genres.splice(0, 5);
+
+        })).then(() => {
+          console.log(media);
+        })
+      }
+    },
+
+    urlGen(type, category, castOrGenres, id) {
+      if (type === 'search') {
+
+        return `${store.apiURL}search/${category}/?${store.apiKey}${store.apiLanguage}&query=${store.searchString}`
+
+      } else {
+
+        return `${store.apiURL}${category}/${id}${castOrGenres}?${store.apiKey}${store.apiLanguage}`
+      }
+
+
+    },
+
+    countryFlagURL(language) {
+      let languageString = language;
+
+      if (language === 'en') {
+        languageString = 'gb';
+      }
+
+      return `https://flagsapi.com/${languageString.toUpperCase()}/flat/64.png`
     }
+
   }
 }
 </script>
@@ -31,23 +86,35 @@ export default {
 <template>
   <input type="text" v-model="store.searchString">
 
-  <button
-    @click="searchContent('movie', store.searchResultMovie); searchContent('tv', store.searchResultTV);">Cerca</button>
+  <button @click="searchContent()">Cerca</button>
 
-  <ul v-for="content in store.searchResultMovie">
-    MOVIE
-    <li>{{ content.title }}</li>
-    <li>{{ content.original_title }}</li>
-    <li>{{ content.original_language }}</li>
-    <li>{{ content.vote_average }}</li>
-  </ul>
+  <ul v-for="content in store.searchResult">
+    {{ content.hasOwnProperty('title') ? 'MOVIE' : 'TV SHOW' }}
 
-  <ul v-for="content in store.searchResultTV">
-    TV SHOW
-    <li>{{ content.name }}</li>
-    <li>{{ content.original_name }}</li>
-    <li>{{ content.original_language }}</li>
+    <img :src="store.apiPosterURL + store.posterWidth + content.poster_path" alt="img">
+
+    <li>{{ content.hasOwnProperty('title') ? content.title : content.name }}</li>
+
+    <li>{{ content.hasOwnProperty('original_title') ? content.original_title : content.original_name }}</li>
+
+    <li>
+      <img v-if="content.hasOwnProperty('original_language')" :src="this.countryFlagURL(content.original_language)"
+        alt="languageflag">
+    </li>
+
     <li>{{ content.vote_average }}</li>
+
+    <ul>
+      <li v-for="actor in content.cast">
+        {{ actor.name }}
+      </li>
+    </ul>
+
+    <ul>
+      <li v-for="genre in content.genres">
+        {{ genre.name }}
+      </li>
+    </ul>
   </ul>
 </template>
 
--- src/store.js
+++ src/store.js
@@ -3,7 +3,9 @@ import { reactive } from "vue";
 export const store = reactive({
     apiURL: 'https://api.themoviedb.org/3/',
     apiKey: 'api_key=fdb78ecb579abbbdd6e3a1990e76f699',
+    apiLanguage: '&language=it_IT',
     searchString: "",
-    searchResultMovie: [],
-    searchResultTV: [],
+    searchResult: [],
+    apiPosterURL: 'https://image.tmdb.org/t/p/',
+    posterWidth: 'w342',
 })
