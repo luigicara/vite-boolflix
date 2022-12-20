14e1ef25af1395c1ba217850beff25c1de3120cb
luigicara <luigi.caravelli98@gmail.com>
2022-12-20 17:23:06 +0100

    farStar to fasStar

--- src/main.js
+++ src/main.js
@@ -8,11 +8,11 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 import { faStar } from '@fortawesome/free-regular-svg-icons'
-import { faStar as farStar } from '@fortawesome/free-solid-svg-icons'
+import { faStar as fasStar } from '@fortawesome/free-solid-svg-icons'
 
 
 /* add icons to the library */
-library.add(faStar, farStar)
+library.add(faStar, fasStar)
 
 
 createApp(App)
