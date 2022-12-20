64315e7f94136e462b5a32a12ca530626ea74c24
luigicara <luigi.caravelli98@gmail.com>
2022-12-20 12:01:29 +0100

    completed style on AppHeader

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -15,15 +15,15 @@ export default {
         <h1>Boolflix</h1>
 
         <div>
-            <input type="text" v-model="store.searchString">
-
-            <button @click="$emit('search')">Cerca</button>
-
             <select name="genres" id="genres" v-if="store.genresSelect.length > 0" v-model="store.genresString">
                 <option v-for="genre in store.genresSelect" :value="genre">
                     {{ genre }}
                 </option>
             </select>
+
+            <input type="text" v-model="store.searchString">
+
+            <button @click="$emit('search')">Cerca</button>
         </div>
     </header>
 </template>
@@ -33,11 +33,40 @@ header {
     display: flex;
     justify-content: space-between;
     align-items: center;
-    height: 10rem;
+    height: 6rem;
     background-color: black;
 
     h1 {
+        margin: 1rem;
+        font-size: 4rem;
         color: red;
+    }
+
+    div {
+        margin: 3rem;
+
+        select {
+            color: white;
+            background-color: black;
+            border: 2px solid red;
+            border-radius: 1rem;
+        }
+
+        input {
+            margin: 1rem;
+            padding: 0.3rem;
+            border: 2px solid red;
+            border-radius: 1rem;
+        }
+
+        button {
+            font-size: 1rem;
+            color: white;
+            padding: 0.4rem;
+            border: 2px solid red;
+            border-radius: 1rem;
+            background-color: black;
+        }
     }
 }
 </style>
